1b72f625bae4fffc00971e50f7394b968a23de2c
Mirabror <99737165+0101mirabror@users.noreply.github.com>
2023-02-10 22:50:46 +0500

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(i=hg1k40os#q^0%$*3h+1j$($%v)lseuoc=whnxcohvd+9u%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com","localhost", "127.0.0.1"]  #new
 
 
 # Application definition
@@ -36,14 +36,19 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic", #new
     "django.contrib.staticfiles",
+    #3rd party
+    "rest_framework",          #new
     # Local
-    "books.apps.BooksConfig",
+    "books.apps.BooksConfig",       #new
+    "apis.apps.ApisConfig",     #new
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", #new
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -118,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"] #new
+STATIC_ROOT = BASE_DIR / "staticfiles" #new
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
